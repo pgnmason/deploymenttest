f45046fad391580740a32091e06e5ddb8b15147d
pgnmason <nmason@post-gazette.com>
2017-07-06 23:26:12 -0400

    Making Zip Code Locator Work as Ajax Request

--- api/zipcode/index.php
+++ api/zipcode/index.php
@@ -4,20 +4,37 @@ $config_path = dirname(__FILE__)."/../flip/app/php/config/";
 $inc_root = dirname(__FILE__)."/../flip/app/php";
 
 require(dirname(__FILE__)."/../flip/app/php/files/functions.php"); 
+loadComponent("ajaxresponse");
+$response = new AjaxResponse();
+
 if(!empty($_GET['zip']) && is_numeric($_GET['zip'])){
+	$zip_code = Request::getVar("zip","get",false);
+	$zip_code = trim($zip_code);
+	if(!$zip_code){
+		$response->code=433;
+		$response->message="Missing Parameters";
+	}else if(!is_numeric($zip_code)){
+		$response->code = 431;
+		$response->message = "Invalid Parameters";
+	}else{
+		$db = Factory::getDBO();
+	 	$zip_code = $db->sanitize($zip_code);
+	 	$db->setQuery("SELECT * FROM zip_codes WHERE zip_code = ".$zip_code." LIMIT 1");
+	 	$data = $db->loadObject();
 
-	var_dump($_GET['zip']);
-	$db = Factory::getDBO();
- 	/*if($c = self::checkCache($zip_code,$radius) && false){
- 		return $c;
- 	}*/
- 	$zip_code = $db->sanitize($zip_code);
- 	$db->setQuery("SELECT * FROM zip_codes WHERE zip_code = ".$zip_code." LIMIT 1");
- 	$loc = $db->loadObject();
-
- 	//echo $db->getQuery();
- 	echo json_encode($loc);
-}else{
-	die("SOMETHING IS WRONG");
+		if($data){
+			$response->code=200;
+			$response->status="ok";
+			$response->message="success";
+			$response->data = $data;
+		}else{
+			$response->code=501;
+			$response->message="Internal Error";
+		}
+	} 	//echo $db->getQuery();
 }
+
+header('Content-Type: application/json');
+echo json_encode($response)
+
 ?>
